98d3797ee022dc5bfa60a316de7ed4f34645eaf3
Isla-the-programmer <isla.turukpaev@gmail.com>
2018-10-07 22:29:21 +0300

    assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt

--- assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt
+++ assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt
@@ -4,17 +4,24 @@ package io.rybalkinsd.kotlinbootcamp.geometry
  * Entity that can physically intersect, like flame and player
  */
 interface Collider {
-    fun isColliding(other: Collider): Boolean
+    fun isColliding(other: Collider) : Boolean
 }
 
 /**
  * 2D point with integer coordinates
  */
-class Point(x: Int, y: Int) : Collider {
-    override fun isColliding(other: Collider): Boolean {
-        TODO("not implemented")
+class Point(val x: Int, val y: Int) : Collider {
+    override fun isColliding(other: Collider) = when (other) {
+        is Point -> this == other
+        is Bar -> other.isColliding(this)
+        else -> false
+    }
+    override fun equals(other: Any?)=  when(other) {
+        is Point -> (this.x == other.x) and (this.y == other.y)
+        else -> false
     }
 }
+
 
 /**
  * Bar is a rectangle, which borders are parallel to coordinate axis
@@ -22,8 +29,27 @@ class Point(x: Int, y: Int) : Collider {
  * Bar is not oriented
  * (It does not matter, which opposite corners you choose to define bar)
  */
-class Bar(firstCornerX: Int, firstCornerY: Int, secondCornerX: Int, secondCornerY: Int) : Collider {
-    override fun isColliding(other: Collider): Boolean {
-        TODO("not implemented")
+class Bar(val firstCornerX: Int, val firstCornerY: Int, val secondCornerX: Int, val secondCornerY: Int) : Collider {
+    override fun isColliding(other: Collider) = when(other) {
+        is Point -> {
+            val list1 = listOf(firstCornerX,secondCornerX).sorted()
+            val list2 = listOf(firstCornerY,secondCornerY).sorted()
+            (list1[0] <= other.x) and (other.x <= list1[1]) and (list2[0] <= other.y) and (other.y <= list2[1])
+        }
+        is Bar -> {
+            val list1 = listOf(firstCornerX,secondCornerX).sorted()
+            val list2 = listOf(firstCornerY,secondCornerY).sorted()
+            val list3 = listOf(other.firstCornerX, other.secondCornerX).sorted()
+            val list4 = listOf(other.firstCornerY, other.secondCornerY).sorted()
+            val log1 = if (list1[0] < list3[0]) (list1[1] >= list3[0]) else (list1[0] <= list2[1])
+            val log2 = if (list2[0] < list4[0]) (list2[1] >= list4[0]) else (list2[0] <= list4[1])
+            log1 and log2
+        }
+        else -> false
+    }
+    override fun equals(other: Any?)=  when(other) {
+        is Bar -> listOf(firstCornerX, secondCornerX).containsAll(listOf(other.firstCornerX, other.secondCornerX)) and
+                listOf(firstCornerY, secondCornerY).containsAll(listOf(other.firstCornerY, other.secondCornerY))
+        else -> false
     }
 }
